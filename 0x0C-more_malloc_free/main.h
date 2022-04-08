c94a4d0c0c1fc844c344a25ac7999eedb8f2f9f3
lucymell <99334158+lucymell@users.noreply.github.com>
2022-04-08 18:37:51 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,31 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-void _puts(char *str);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * struct add_s - Node in the linked list of additions
+ *
+ * @n_add: Pointer to an addition representation
+ * @n_dig: Real space that addtion takes
+ * @len_r: Total space to save the addition
+ * @next: Pointer to the next addition
+ */
+typedef struct add_s
+{
+	char *n_add;
+	int n_dig;
+	int len_r;
+	struct add_s *next;
+} add_t;
+
+
+int _putchar(char c);  /* An example function declaration */
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
-
-
 #endif
